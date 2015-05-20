6dd68bf5a4a9c69f55a2c224bc543adce4df5a34
samvaran kashyap <samvaran2best@gmail.com>
2015-05-20 15:52:47 -0500

    stable commit

--- project.py
+++ project.py
@@ -18,7 +18,8 @@ def main_menu():
                 add_new_transaction()
             elif input_text =='D':
                 print "Deleting Last transaction\n"
-                delete_last_transaction()
+                c_id = raw_input("Enter customer id:\n")
+                delete_last_transaction(c_id)
             elif input_text== 'Q':
                 print "Quiting ..."  
                 sys.exit(0) 
@@ -27,21 +28,24 @@ def main_menu():
 
 def add_new_transaction():
     print "new transaction added\n"
-    cust_id = raw_input("Enter customer id:\n")
-    total_sale_amt = raw_input("Enter total Sale amount")
-    discount_amount = calculate_discount(total_sale_amt)
-    discount_sale_amt = total_sale_amt - discount_amount
-    reward_points = calculate_reward_points(total_sale_amt)
-    additional_discount = calculate_additional_discount(cust_id,reward_points)
-    sale_after_add_disc = discount_sale_amt-additional_discount
-    cu_sale_for_reward = next_reward_sale(cust_id)
-    final_sale_amount = sale_after_add_disc
-    write_to_transactions(cust_id,total_sale_amt,discount_amount,discount_sale_amt,reward_points,additional_discount, sale_after_add_disc,cu_sale_for_reward, final_sale_amount)
+    attr_dict = {}
+    attr_dict["cust_id"]= raw_input("Enter customer id:\n")
+    attr_dict["total_sale_amt"]=  raw_input("Enter total Sale amount")
+    attr_dict["discount_amount"]= calculate_discount(attr_dict["total_sale_amt"])
+    attr_dict["discount_sale_amt"]= int(attr_dict["total_sale_amt"]) - int(attr_dict["discount_amount"])
+    #pdb.set_trace()
+    attr_dict["reward_points"]=  calculate_reward_points(attr_dict["total_sale_amt"])
+    attr_dict["additional_discount"]= calculate_additional_discount(attr_dict["cust_id"],attr_dict["reward_points"])
+    attr_dict["sale_after_add_disc"]=  int(attr_dict["discount_sale_amt"])-int(attr_dict["additional_discount"])
+    attr_dict["cu_sale_for_reward"]=  next_reward_sale(attr_dict["cust_id"])
+    attr_dict["final_sale_amount"] =  attr_dict["sale_after_add_disc"]
+    write_to_transactions(attr_dict)
     
 
 def delete_last_transaction(cust_id):
     print "delete last transaction\n"
     #pdb.set_trace()
+    cust_id = int(cust_id)
     fd = open("sales.txt","r")
     lines = fd.readlines()
     fd.close()
@@ -54,7 +58,7 @@ def delete_last_transaction(cust_id):
         if s1[0]=="Customer ID" and s1[1].strip("\n").strip()==str(cust_id):
             lines_to_remove.append(line_count)
             counter +=1
-    pdb.set_trace()
+    #pdb.set_trace()
     new_lines1 = lines[0:lines_to_remove[-1]-1]
     new_lines2 = lines[lines_to_remove[-1]-1:lines_to_remove[-1]+8]
     new_lines3 = lines[lines_to_remove[-1]+8:line_count]
@@ -71,6 +75,7 @@ def calculate_discount(total_sale_amt):
     print "calculating discount\n"
     #pdb.set_trace()
     discount =0 
+    total_sale_amt = int(total_sale_amt)
     discount_details = get_discount_details()
     for x in discount_details:
         s1 = x.split(" ")
@@ -80,25 +85,43 @@ def calculate_discount(total_sale_amt):
         if "From" == s1[0]:
             if total_sale_amt < int(s1[6].strip("$").replace(",","")) and total_sale_amt >= int(s1[1].strip("$").replace(",","")):
                 if len(s1)== 9:
-                    discount = total_sale_amt * int(s1[8].strip('\n').strip("%"))/100
+                    discount = (total_sale_amt-int(s1[1].strip('\n').strip("$")))*int(s1[8].strip('\n').strip("%"))/100
                 else:
-                    discount = total_sale_amt * int(s1[10].strip('\n').strip("%"))/100
+                    discount = (total_sale_amt-int(s1[1].strip('\n').strip("$"))) * int(s1[10].strip('\n').strip("%"))/100
                     discount+= int(s1[8].strip("$"))
         if "above" in s1:
             if total_sale_amt >= int(s1[0].strip("$").replace(",","")):
-                discount = total_sale_amt * int(s1[6].strip('\n').strip("%"))/100
+                discount = (total_sale_amt-int(s1[0].strip('\n').strip("$"))) * int(s1[6].strip('\n').strip("%"))/100
                 discount+= int(s1[4].strip("$"))
     print discount
     return discount
 
 def calculate_reward_points(total_sale_amt):
     print "calculating reward points"
-    reward = total_sale_amt/10
-    reward = math.ceil(reward)
-    print reward
+    total_sale_amt = int(total_sale_amt)
+    reward = total_sale_amt/100
+    reward = math.floor(reward)
+    #print reward
+    return reward
 
-def write_to_transactions(cust_id,total_sale_amt,discount_amount,discount_sale_amt,reward_points,additional_discount, sale_after_add_disc,cu_sale_for_reward, final_sale_amount):
+def write_to_transactions(attr_dict):
     print "writing to file"
+    print attr_dict
+    fd = open("sales.txt","a")
+    p_str = ""
+    p_str += "Customer ID: "+str(attr_dict["cust_id"])+"\n"
+    p_str += "Total sale amount: "+str(attr_dict["total_sale_amt"])+"\n"
+    p_str += "Discount amount: "+str(attr_dict["discount_amount"])+"\n"
+    p_str += "Discount sale amount: "+str(attr_dict["discount_sale_amt"])+"\n"
+    p_str += "Reward point: "+str(attr_dict["reward_points"])+"\n"
+    p_str += "Additional discount: "+str(attr_dict["additional_discount"])+"\n"
+    p_str += "Additional discounted sale amount: "+str(attr_dict["sale_after_add_disc"])+"\n"
+    p_str += "Cumulative sale amount for the next reward point: "+str(attr_dict["cu_sale_for_reward"])+"\n"
+    p_str += "Final sale amount: "+str(attr_dict["final_sale_amount"])+"\n"
+    print p_str
+    fd.write(p_str)
+    fd.close()
+    
 
 def get_discount_details():
     fd = open("discount.txt",'r')
@@ -107,6 +130,9 @@ def get_discount_details():
 
 def calculate_additional_discount(cust_id,reward_points):
     r_points = 0 
+    add_discount = 0
+    cust_id = int(cust_id)
+    reward_points = int(reward_points)
     fd = open("sales.txt","r")
     lines = fd.readlines()
     i =0
@@ -115,18 +141,26 @@ def calculate_additional_discount(cust_id,reward_points):
        s1= lines[i]
        s1 = s1.split(":")
        if s1[0]=="Customer ID" and s1[1].strip("\n").strip()==str(cust_id):
-           pdb.set_trace()
+           #pdb.set_trace()
            r_line = i+4
            s2 = lines[r_line]
            s2 = s2.split(":")
            r_point = s2[1].strip("\n")
            r_points += int(r_point)
        i+=1
-    print r_points       
-    return r_points
+    print r_points
+    pdb.set_trace()
+    if (r_points+reward_points)/100 > 0 :
+        add_discount = math.floor(r_points+reward_points/100)*10  
+    print add_discount   
+    return add_discount
 
+def next_reward_sale(cust_id):
+    return 10
+
+main_menu()
 #calculate_reward_points(2340)
 #get_discount_details()
 #main_menu()
 #delete_last_transaction(1)
-calculate_additional_discount(3,233)
+#calculate_additional_discount(2,3233)
